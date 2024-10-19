0b428e522e39ba095901e965128eac6a74bd93c9
ringsaturn <ringsaturn.me@gmail.com>
2024-10-19 16:04:57 +0800

    update

--- src/lib.rs
+++ src/lib.rs
@@ -1,13 +1,27 @@
-use lazy_static::lazy_static;
+use wasm_bindgen::prelude::*;
 use tzf_rs::DefaultFinder;
-use wasm_bindgen::{prelude::wasm_bindgen, JsValue};
 
-lazy_static! {
-    static ref FINDER: DefaultFinder = DefaultFinder::default();
+#[wasm_bindgen]
+pub struct WasmFinder {
+    finder: DefaultFinder,
 }
 
-#[wasm_bindgen(js_name = getTz)]
-pub fn get_tz_name(lng: f64, lat: f64) -> JsValue {
-    let tz = FINDER.get_tz_name(lng, lat);
-    JsValue::from_str(&tz)
+#[wasm_bindgen]
+impl WasmFinder {
+    #[wasm_bindgen(constructor)]
+    pub fn new() -> WasmFinder {
+        WasmFinder {
+            finder: DefaultFinder::default(),
+        }
+    }
+
+    #[wasm_bindgen]
+    pub fn get_tz(&self, lng: f64, lat: f64) -> String {
+        self.finder.get_tz_name(lng, lat).to_string()
+    }
+
+    #[wasm_bindgen]
+    pub fn data_version(&self) -> String {
+        self.finder.data_version().to_string()
+    }
 }
